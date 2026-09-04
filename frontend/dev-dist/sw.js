4bdf1310c263ab003baabe206dae20280094d621
mshirazkamran <dev.mshiraz@gmail.com>
2026-09-04 15:07:31 +0500

    prep: preparing for deployment

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.ovhonms3414"
+    "revision": "0.f49ir44ocgs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
